38e25953ab66cfab05a2a592b1d224f7c950cf4f
Pablo DS <pabloosancheez0@gmail.com>
2024-02-08 14:57:26 -0600

    Cyberchase DB

--- Week 0 Querying/Cyberchase/1.sql
+++ Week 0 Querying/Cyberchase/1.sql
@@ -1,4 +1,4 @@
--- Write a SQL query to list the titles of all episodes in Cyberchase’s original season, Season 1.
+-- Write a SQL query to list the titles of all episodes in Cyberchase’s original season (Season 1).
 
 SELECT title
 FROM episodes
